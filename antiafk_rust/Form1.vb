5f18dd03fa86247cda1ffaca8be266dd67824ca3
pede <pede@team-xp.dk>
2017-07-27 16:00:23 +0200

    Add files via upload

---
+++ antiafk_rust/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Net
+Imports System.IO
+Public Class Form1
+    Dim run_ctrl As Boolean = False
+    Dim on_var As Boolean = False
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If on_var = False Then
+            Timer1.Enabled = True
+            on_var = True
+            Button1.Text = "Stop"
+        Else
+            Timer1.Enabled = False
+            on_var = False
+            Button1.Text = "Start"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim index As Integer = 0
+        Do While index <= 100
+            SendKeys.Send("{D}")
+            index += 1
+        Loop
+        If run_ctrl = False Then
+            run_ctrl = True
+        Else
+            run_ctrl = False
+        End If
+    End Sub
+End Class
---
+++ antiafk_rust/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("antiafk_rust")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("antiafk_rust")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07c101de-97da-4e3e-8f8a-d8c852f0e855")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
